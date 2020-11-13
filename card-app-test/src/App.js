dd99fbbada7e0531e1cac4a0c25e663b3b7fa1c2
petarchord <petarchord@hotmail.com>
2020-11-13 21:55:56 +0100

    show cards from local storage logic done

--- card-app-test/src/App.js
+++ card-app-test/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import styles from "./App.module.scss";
 import {Cards , AddCard} from "./components";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 function App() {
   return (
-    <div className={styles.container}>
+
+    <Router>
+<div className={styles.container}>
+      <Switch>
+        <Route path="/" exact component={Cards} />
+        <Route path="/cards/add" component={AddCard} />
+      </Switch>
       <Cards />
       <AddCard />
     </div>
+    </Router>
+    
   );
 }
 
--- card-app-test/src/components/Cards/Cards.jsx
+++ card-app-test/src/components/Cards/Cards.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import styles from "./Cards.module.scss";
+import Card from "../Card/Card";
 
 const Cards = () => {
+  const renderAvailableCard = () => {
+    if (localStorage.getItem("cardData")) {
+      const {
+        cardType,
+        userName,
+        firstDigits,
+        secondDigits,
+        thirdDigits,
+        fourthDigits,
+        expDate,
+      } = JSON.parse(localStorage.getItem("cardData"));
+      return (
+        <Card
+          cardType={cardType}
+          userName={userName}
+          firstDigits={firstDigits}
+          secondDigits={secondDigits}
+          thirdDigits={thirdDigits}
+          fourthDigits={fourthDigits}
+          expDate={expDate}
+        />
+      );
+    }
+  };
   return (
     <div className={styles.container}>
       <h3>My Cards</h3>
-      <div className={styles.add_card}>
-        <p>+</p>
-      </div>
+      {renderAvailableCard()}
+      {renderAvailableCard() ? (
+        ""
+      ) : (
+        <div className={styles.add_card}>
+          <p>+</p>
+        </div>
+      )}
     </div>
   );
 };
